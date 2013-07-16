df2210172ee90945c28d1ab1ccd4a84d03f77906
Philip Champon <philip@adaptly.com>
2013-07-16 14:34:38 -0400

    Added support for sql functions

--- lib/httpsql/version.rb
+++ lib/httpsql/version.rb
@@ -1,3 +1,3 @@
 module Httpsql
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,25 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'active_record'
+require 'httpsql'
+
+ActiveRecord::Base.configurations[:test] = {adapter:  'sqlite3', database: 'tmp/httpsql_test'}
+ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[:test])
+ActiveRecord::Base.connection.execute %Q{ DROP TABLE IF EXISTS foo_models }
+ActiveRecord::Base.connection.execute %Q{ 
+  CREATE TABLE foo_models (
+    id integer,
+    int_field integer,
+    dec_field decimal,
+    string_field text,
+    access_token text,
+    created_at text default CURRENT_TIMESTAMP,
+    updated_at text default CURRENT_TIMESTAMP,
+    primary key(id)
+  );
+}
+class FooModel < ActiveRecord::Base
+  include Httpsql
+  attr_accessible :int_field, :dec_field, :string_field, :access_token
+end
+
