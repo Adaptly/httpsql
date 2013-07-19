3c613a9c48e973962acee16a4eb75b6e2965bd7a
Philip Champon <philip@adaptly.com>
2013-07-19 14:59:39 -0400

    Major improvments
    - Refactored method names
      - `where_params_eq` is now `with_params`
    	- `route_params` is now `grape_documentation`
    - Support for group, order, join
    - Replaced grape grape-0.4.x with grape-0.5.x
    - Coveralls works, I think?
    - Corrected docs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
+require 'simplecov'
+#SimpleCov.start
+require 'coveralls'
+Coveralls.wear!
+
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'active_record'
+require 'grape'
 require 'httpsql'
-
-require 'simplecov'
-require 'coveralls'
-Coveralls.wear!
 
 ActiveRecord::Base.configurations[:test] = {adapter:  'sqlite3', database: 'tmp/httpsql_test'}
 ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[:test])
@@ -22,8 +24,79 @@ ActiveRecord::Base.connection.execute %Q{
     primary key(id)
   );
 }
+
+ActiveRecord::Base.connection.execute %Q{ DROP TABLE IF EXISTS bar_models }
+ActiveRecord::Base.connection.execute %Q{ 
+  CREATE TABLE bar_models (
+    id integer,
+    foo_model_id integer,
+    string_field text,
+    primary key(id)
+  );
+}
+
+ActiveRecord::Base.connection.execute %Q{ DROP TABLE IF EXISTS baz_models }
+ActiveRecord::Base.connection.execute %Q{ 
+  CREATE TABLE baz_models (
+    id integer,
+    foo_model_id integer,
+    string_field text,
+    primary key(id)
+  );
+}
+
+ActiveRecord::Base.connection.execute %Q{ DROP TABLE IF EXISTS bam_models }
+ActiveRecord::Base.connection.execute %Q{ 
+  CREATE TABLE bam_models (
+    id integer,
+    bar_model_id integer,
+    string_field text,
+    primary key(id)
+  );
+}
+
 class FooModel < ActiveRecord::Base
   include Httpsql
-  #attr_accessible :int_field, :dec_field, :string_field, :access_token
+  has_one :bar_model
+  has_many :baz_models
+end
+
+class BarModel < ActiveRecord::Base
+  include Httpsql
+  belongs_to :foo_model
+  has_many :bam_models
+end
+
+class BazModel < ActiveRecord::Base
+  include Httpsql
+  belongs_to :foo_model
+end
+
+class BamModel < ActiveRecord::Base
+  include Httpsql
+  belongs_to :bar_model
+end
+
+class TestApi < Grape::API
+  resource :foo_models do
+    desc 'foo models index'
+    params do
+      FooModel.grape_documentation(self)
+    end
+    get '/' do
+      FooModel.with_params(params)
+    end
+  end
+
+  resource :baz_models do
+    desc 'baz models index'
+    params do
+      BazModel.grape_documentation(self)
+    end
+    get '/' do
+      BazModel.with_params(params)
+    end
+  end
+
 end
 
